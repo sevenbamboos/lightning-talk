2567e92e82fb02e42e351d5cc780a3372534046c
Sam Wang <sam.wang@fostermoore.com>
2019-01-30 09:53:26 +1300

    add back db script

---
+++ server/db.mock.sql
@@ -0,0 +1,6 @@
+INSERT INTO `topics` (`id`, `title`, `description`, `email`, `created_at`, `talk_time`, `ip_addr`) VALUES 
+(1, 'Find bugs', 'dummy topic', 'sevenbamboos@gmail.com', '2019-01-28 21:11:40', '2019-02-07 09:00:00', '192.168.0.35'),
+(2, 'Review code', 'dummy topic', 'sevenbamboos@gmail.com', '2019-01-28 21:10:40', '2019-02-07 09:00:00', '192.168.0.35'),
+(3, 'Fix bugs', 'dummy topic', 'sevenbamboos@gmail.com', '2019-01-28 21:09:40', '2019-02-07 09:00:00', '192.168.0.35'),
+(4, 'Refactor code', 'dummy topic', 'sevenbamboos@gmail.com', '2019-01-28 21:08:40', '2019-02-07 09:00:00', '192.168.0.35'),
+(5, 'Push to prod', 'dummy topic', 'sevenbamboos@gmail.com', '2019-01-28 21:07:40', '2019-02-07 09:00:00', '192.168.0.35');
