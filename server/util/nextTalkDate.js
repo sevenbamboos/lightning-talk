ea234feb2ac012a30556399c22210a8450cbbabf
Sam Wang <sam.wang@fostermoore.com>
2019-01-30 09:46:19 +1300

    move db script

--- server/util/nextTalkDate.js
+++ server/util/nextTalkDate.js
@@ -21,7 +21,7 @@ const firstThursdays = year => {
 
   const firsts = [1, 3, 5, 7, 9, 11].map(x => new Date(year, x, 1));
   const diffs = firsts.map(first => {const diff = 4 - first.getDay(); return diff > 0 ? diff : 7 + diff;});
-  const zip= rows=>rows[0].map((_,c)=>rows.map(row=>row[c]));
+  const zip = rows => rows[0].map((_,c) => rows.map(row => row[c]));
   const firstsWithDiffs = zip([firsts, diffs]);
   const thursdays = firstsWithDiffs.map(z => addDaysTo(z[0], z[1]));
 
