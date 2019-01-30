fb2a72e87daebf508c676336ebbd2e15f780bd82
sevenbamboos <sevenbamboos@gmail.com>
2019-01-31 00:04:34 +1300

    What: fine-tune topic edit
    Why:
    
    How:

--- src/app/topic.ts
+++ src/app/topic.ts
@@ -2,8 +2,8 @@ export class Topic {
   // id: number;
   // title: string;
   // description: string;
-  submissionDate: Date;
-  talkDate: Date;
+  created_at: Date;
+  talk_time: Date;
 
   constructor(public id: number, public title: string, public email: string, public description: string) {
   }
